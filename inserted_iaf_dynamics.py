0f12bd1a6383e094a1084ec6fba61d211743a388
nehadas <neha.das@tum.de>
2017-10-15 08:44:39 +0200

    Commit inserted_iaf

---
+++ inserted_iaf_dynamics.py
@@ -0,0 +1,233 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.contrib.distributions import MultivariateNormalDiag, kl_divergence
+from base import Mlp, AR_Net
+import utils
+
+
+#TODO: Check :
+# comparison with baseline
+# variance=1
+# come up with an environment with multimodal posteriors
+
+
+class BaselineTransitionNoKL(object):
+
+    def __init__(self, n_latent, n_enc, n_control, n_secenc = 10):
+
+        self.n_latent = n_latent
+        self.n_enc = n_enc
+        self.n_control = n_control
+
+        # Mlp for posterior and prior
+        z_size_q = self.n_latent
+        h_size_q = self.n_enc + self.n_control
+        n_henc = n_secenc           #n_secenc indicates the number of dimensions for encoding [z, enc, u] for creating h for iaf
+        n_input_q = self.n_enc + self.n_latent + self.n_control
+        n_input_p = self.n_latent + self.n_control
+        n_output = self.n_latent + self.n_latent
+        layers = [256, 256]
+        activation = ['relu', 'relu']
+        out_activation = lambda x: x
+
+        #TODO: Check if the implementations for q_henc and q_iaf are correct
+
+        self.q_iaf = AR_Net(z_size = z_size_q, h_size = n_henc, shape = layers)
+        self.q_henc = Mlp(n_input_q, layers, n_henc, activation, out_activation)
+        self.q_mlp = Mlp(n_input_q, layers, n_output, activation, out_activation)
+        self.p_mlp = Mlp(n_input_p, layers, n_output, activation, out_activation)
+
+    def q_transition_IAF(self, z, h):
+
+        # Get mean and var
+        var_f0, z_temp = self.q_iaf(h,z)
+
+        return z_temp, var_f0
+
+    def q_transition(self, z, enc, u):
+
+        # Concat the inputs
+        zeu = tf.concat([z, enc, u], 1)
+
+        # Get mean and var
+        out = self.q_mlp(zeu)
+        mean = out[:, :self.n_latent] # first n_latent vars are mean
+        var = out[:, self.n_latent:] # next n_latent vars are variance
+
+        return mean, var ** 2 + 1e-5
+
+    def p_transition(self, z, u):
+
+        # Concat the inputs
+        zu = tf.concat([z, u], 1)
+
+        # Get mean and var
+        out = self.p_mlp(zu)
+        mean = out[:, :self.n_latent]
+        var = out[:, self.n_latent:]
+
+        return mean, var ** 2 + 1e-5
+
+    def one_step_IAF(self, a, x):
+        z = a[0]
+        #log_q = a[1]
+        u, enc = x
+
+        input_h = tf.concat([z, enc, u], 1)                          #input should have enc(x), u and previous z
+        h = self.q_henc(input_h)                                     #h encoding for iaf
+        q_mean, q_var = self.q_transition(z, enc, u)
+        p_mean, p_var = self.p_transition(z, u)
+
+        q = MultivariateNormalDiag(q_mean, tf.sqrt(q_var))
+        p = MultivariateNormalDiag(p_mean, tf.sqrt(p_var))
+
+        z_step = q.sample()
+
+        log_q = q.log_prob(z_step)                                  #before performing the iaf step
+
+        z_step_iaf, q_var = self.q_transition_IAF(z_step, h)
+        log_q = log_q - tf.reduce_sum(tf.log(q_var + 1e-5), axis=1) #after performing the iaf step
+
+        log_p = p.log_prob(z_step_iaf)  #TODO: check if this is correct? Should we be getting the probability of z_step or z_step_iaf?
+
+        return z_step_iaf, log_q, log_p
+
+    def gen_one_step(self, z, u):
+
+        p_mean, p_var = self.p_transition(z, u)
+        
+        p = MultivariateNormalDiag(p_mean, tf.sqrt(p_var))
+
+        z_step = p.sample()
+
+        return z_step
+
+
+class DVBFNoKL():
+    def __init__(self, n_obs, n_control, n_latent, n_enc, chkpoint_file=None):
+
+        self.learning_rate = tf.placeholder(tf.float32)
+        self.annealing_rate = tf.placeholder(tf.float32)
+        
+        # Dimensions
+        self.n_output = n_obs
+        self.n_obs = n_obs
+        self.n_control = n_control
+        self.n_latent = n_latent
+        self.n_enc = n_enc
+
+        # The placeholder from the input
+        self.x = tf.placeholder(tf.float32, [None, None, self.n_obs], name="X")
+        self.u = tf.placeholder(tf.float32, [None, None, self.n_control], name="U")
+
+        # Initialize p(z0), p(x|z), q(z'|enc, u, z) and p(z'|z) as well as the mlp that
+        # generates a low dimensional encoding of x, called enc
+        self._init_generative_dist()
+        self._init_start_dist()
+        self._init_encoding_mlp()
+        self.transition = BaselineTransitionNoKL(self.n_latent, self.n_enc, self.n_control)
+        
+        # Get the encoded representation of the observations (this makes sense when observations are highdimensional images for example)
+        enc = self.get_enc_rep(self.x)
+        
+        # Get the latent start state
+        q0 = self.get_start_dist(self.x[0])
+        z0 = q0.sample()
+        log_q0 = q0.log_prob(z0)
+        p0 = MultivariateNormalDiag(tf.zeros(tf.shape(z0)), tf.ones(tf.shape(z0)))
+        log_p0 = p0.log_prob(z0)
+                               
+        # Trajectory rollout in latent space + calculation of KL(q(z'|enc, u, z) || p(z'|z))
+        z, log_q, log_p = tf.scan(self.transition.one_step_IAF, (self.u[:-1], enc[1:]),  (z0, log_q0, log_p0))
+        self.z = tf.concat([[z0], z], 0)
+        
+        # Get the generative distribution p(x|z) + calculation of the reconstruntion error
+        # TODO: Including x[0], revert if doesn't work
+        # px = self.get_generative_dist(z)
+        # rec_loss = -px.log_prob(self.x[1:])
+
+        # TODO: Including x[0], Remove if doesn't work
+        px = self.get_generative_dist(self.z)
+        rec_loss = -px.log_prob(self.x)
+
+        self.px_mean = px.mean()
+        
+        # Generating trajectories given only an initial observation
+        gen_z = tf.scan(self.transition.gen_one_step, self.u[:-1], z0)
+        self.gen_z = tf.concat([[z0], gen_z], 0)
+        gen_px = self.get_generative_dist(self.gen_z)
+        self.gen_x_mean = gen_px.mean()
+
+        # TODO: Including x[0], Remove if doesn't work
+        log_p = tf.concat([[log_p0], log_p], 0)
+        log_q = tf.concat([[log_q0], log_q], 0)
+
+        # Create the losses
+        # self.rec_loss = rec_loss
+        self.log_p = log_p * self.annealing_rate
+        self.log_q = log_q * self.annealing_rate
+        self.rec_loss = tf.reduce_mean(rec_loss)
+        self.kl_loss = tf.reduce_mean( self.log_q - self.log_p)
+
+
+
+        # self.total_loss = tf.reduce_mean(rec_loss + self.log_q - self.log_p)
+        self.total_loss = self.total_loss = self.kl_loss + self.rec_loss
+
+        # Use the Adam optimizer with clipped gradients
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+        grads_and_vars = self.optimizer.compute_gradients(self.total_loss)
+        capped_grads_and_vars = [(tf.clip_by_value(grad, -1, 1), var) if grad is not None else (grad, var) for 
+                                 (grad, var) in grads_and_vars]
+        self.optimizer = self.optimizer.apply_gradients(capped_grads_and_vars)
+
+        # Save weights
+        self.saver = tf.train.Saver(tf.global_variables(), max_to_keep=None)
+
+        # Launch the session
+        self.sess = tf.InteractiveSession()
+        self.sess.run(tf.global_variables_initializer())
+        if chkpoint_file:
+            utils.load_checkpoint(self.sess, chkpoint_file)
+        
+    def _init_start_dist(self):
+        self.init_mlp = Mlp(self.n_obs, [128], 2 * self.n_latent, ['relu'], lambda x: x)
+        
+    def _init_encoding_mlp(self):
+        self.encoding_mlp = Mlp(self.n_obs, [128], self.n_enc, ['relu'], lambda x: x)
+        
+    def _init_generative_dist(self):
+        self.generative_mlp = Mlp(self.n_latent, [128], self.n_obs, ['relu'], lambda x: x)
+        self.x_var = tf.Variable(tf.random_normal((1, self.n_obs)) * 0.001)
+        
+    def get_start_dist(self, obs0):
+        out = self.init_mlp(obs0)
+        z0_mean = out[:, :self.n_latent]
+        z0_var = out[:, self.n_latent:] ** 2 + 1e-5
+        return MultivariateNormalDiag(z0_mean, tf.sqrt(z0_var))
+    
+    def get_enc_rep(self, x):
+        enc = self.encoding_mlp(tf.reshape(x, (-1, self.n_obs)))
+        enc = tf.reshape(enc, (tf.shape(x)[0], -1, self.n_enc))
+        return enc
+        
+    def get_generative_dist(self, z):
+        x_mean = self.generative_mlp(tf.reshape(z, (-1, self.n_latent)))
+        x_mean = tf.reshape(x_mean, (tf.shape(z)[0], -1, self.n_obs))
+        x_var = tf.zeros_like(x_mean) + self.x_var ** 2 + 1e-8
+        x_var = tf.reshape(x_var, (tf.shape(z)[0], -1, self.n_obs))
+        
+        return MultivariateNormalDiag(x_mean, tf.sqrt(x_var))
+
+    def save(self, name='dvbf', global_step=0):
+        return self.saver.save(self.sess, name, global_step)
+
+    def restore(self, path):
+        self.saver.restore(self.sess, path)
+        
+    def train(self, batch_x, batch_u, learning_rate, annealing_rate):
+        _, total_loss, rec_loss_val, kl_loss_val = self.sess.run((self.optimizer, self.total_loss, self.rec_loss, self.kl_loss), feed_dict={self.x: batch_x,
+                                                                                   self.u: batch_u,
+                                                                                   self.learning_rate: learning_rate,
+                                                                                   self.annealing_rate: annealing_rate})
+        return total_loss, rec_loss_val, kl_loss_val
---
+++ training_and_evaluation_inserted_iaf.py
@@ -0,0 +1,178 @@
+import tensorflow as tf
+import numpy as np
+
+import time
+import matplotlib.pyplot as plt
+#% matplotlib inline
+import seaborn as sns
+
+import cPickle as pickle
+
+from IPython import display
+
+from inserted_iaf_dynamics import DVBFNoKL
+from world import PendulumFullObs
+
+
+def sample_batch(X, U, batch_size):
+    sample = np.random.rand(batch_size) * X.shape[1]
+    sample = sample.astype(int)
+
+    batch_x = X[:, sample]
+    batch_u = U[:, sample]
+
+    return batch_x, batch_u
+
+# Get the dataset
+# world = PendulumFullObs()
+# X, U, R, S = world.get_data_set(episodes=1000, steps=50)
+# pickle.dump([X, U, R, S], open("data/training_set.p", "wb"))
+X, U, R, S = pickle.load(open("data/training_set.p", "rb"))
+
+# Define the model
+n_obs = 3
+n_control = 1
+n_latent =  4
+n_enc = 10
+chkpoint_file = None
+m = DVBFNoKL(n_obs, n_control, n_latent, n_enc, chkpoint_file=chkpoint_file)
+
+# Training parameters
+training_epochs = 1000
+batch_size = 16
+steps_per_epoch = X.shape[1] / batch_size
+display_step = 10
+chkpt_step = 100
+annealing_epochs = 600
+annealing_rate = 1.0 / annealing_epochs
+learning_rate = 0.001
+min_learning_rate = 0.0002
+decay_rate = 0.99
+
+import utils
+
+timestamp = utils.get_timestamp()
+
+epochs = []
+total_loss = []
+rec_loss = []
+kl_loss = []
+
+import os
+
+_RESULT_PATH = 'inserted_iaf_results/'
+if not os.path.exists(_RESULT_PATH):
+    os.makedirs(_RESULT_PATH)
+
+result_details_path = _RESULT_PATH + '%s' % timestamp + '_result_details.txt'
+result_details_file = open(result_details_path, 'w')
+result_details_file.write('batch_size = ' + str(batch_size) + '\n')
+result_details_file.write('chkpoint_file = ' + str(chkpoint_file) + '\n')
+result_details_file.write('training_epochs = ' + str(training_epochs) + '\n')
+result_details_file.write('annealing_epochs = ' + str(annealing_epochs) + '\n')
+result_details_file.write('initial annealing_rate = ' + str(annealing_rate) + '\n')
+result_details_file.write('initial learning_rate = ' + str(learning_rate) + '\n')
+result_details_file.write('decay_rate = ' + str(decay_rate) + '\n')
+result_details_file.close()
+
+# Training cycle
+for epoch in range(training_epochs):
+
+    # Decay the learning rate
+    if learning_rate > min_learning_rate:
+        learning_rate *= decay_rate
+
+    if annealing_rate < 1.0:
+        annealing_rate += 1.0 / annealing_epochs
+
+    # Train one epoch
+    print("Epoch: ",epoch)
+    avg_total_loss = .0
+    avg_rec_loss = .0
+    avg_kl_loss = .0
+    for i in range(steps_per_epoch):
+        batch_x, batch_u = sample_batch(X, U, batch_size)
+        temp_total_loss, temp_rec_loss, temp_kl_loss = m.train(batch_x, batch_u, learning_rate, annealing_rate)
+        avg_total_loss += temp_total_loss
+        avg_rec_loss += temp_rec_loss
+        avg_kl_loss += temp_kl_loss
+
+    avg_total_loss /= steps_per_epoch
+    avg_rec_loss /= steps_per_epoch
+    avg_kl_loss /= steps_per_epoch
+
+    total_loss.append(avg_total_loss)
+    rec_loss.append(avg_rec_loss)
+    kl_loss.append(avg_kl_loss)
+    epochs.append(epoch)
+
+    #Plot the results
+    if epoch % chkpt_step == 0 or epoch == training_epochs - 1:
+
+        plt.close()
+        f, axarr = plt.subplots(1, 3, figsize=(13, 4))
+        axarr[0].plot(epochs, total_loss)
+        axarr[0].set_xlabel('Epochs')
+        axarr[0].set_title('Total Loss')
+        axarr[0].annotate("Learning Rate: " + str(learning_rate), xy=(0.05, 0.05), xycoords='axes fraction')
+        axarr[0].annotate("Total Loss: " + str(total_loss[-1]), xy=(0.05, 0.0), xycoords='axes fraction')
+        axarr[0].set_ylim(-9.0, 10)
+        axarr[1].plot(epochs, rec_loss)
+        axarr[1].set_xlabel('Epochs')
+        axarr[1].set_title('Rec Loss')
+        axarr[1].annotate("Rec Loss: " + str(rec_loss[-1]), xy=(0.05, 0.0), xycoords='axes fraction')
+        axarr[1].set_ylim(-9.0, 10)
+        axarr[2].plot(epochs, kl_loss)
+        axarr[2].set_xlabel('Epochs')
+        axarr[2].set_title('KL Loss')
+        axarr[2].annotate("KL Loss: " + str(kl_loss[-1]), xy=(0.05, 0.0), xycoords='axes fraction')
+        axarr[2].set_ylim(.0, 10)
+
+        plt.savefig(_RESULT_PATH + '%s' % timestamp + '_Each_Loss_Epoch_' + str(epoch) + '.png')
+
+        # display.display(plt.gcf())
+        # display.clear_output(wait=True)
+
+    if epoch % chkpt_step == 0 or epoch == training_epochs - 1:
+        # save_checkpoint
+        utils.save_checkpoint(sess=m.sess, timestamp=timestamp, checkpoint=epoch)
+
+        plt.close()
+        f, axarr = plt.subplots(1, 1, figsize=(13, 4))
+        axarr.plot(epochs, total_loss)
+        axarr.set_xlabel('Epochs')
+        axarr.set_title('Total Loss')
+        axarr.annotate("Learning Rate: " + str(learning_rate), xy=(0.05, 0.05), xycoords='axes fraction')
+        axarr.annotate("Annealing Rate: " + str(annealing_rate), xy=(0.05, 0.1), xycoords='axes fraction')
+        axarr.annotate("Final Loss: " + str(total_loss[-1]), xy=(0.05, 0.0), xycoords='axes fraction')
+        axarr.set_ylim(-14.0, 10)
+
+        plt.savefig(_RESULT_PATH + '%s' % timestamp + '_Loss_Epoch_' + str(epoch) + '.png')
+
+# Get the true trajectory
+# X_temp, U_temp, R_temp, S_temp = world.get_data_set(episodes=128, steps=50)
+# pickle.dump([X_temp, U_temp, R_temp, S_temp], open("data/test_set.p", "wb"))
+X_temp, U_temp, R_temp, S_temp = pickle.load(open("data/test_set.p", "rb"))
+x_obs = m.sess.run((m.gen_x_mean), feed_dict={m.x: X_temp, m.u:U_temp})
+
+# Plot the position and reward of low dim pendulum
+e = int(np.random.rand() * 100)
+plt.close()
+f, axarr = plt.subplots(1, 2, figsize=(15, 6))
+axarr[0].plot(x_obs[:, e, 0])
+axarr[0].plot(x_obs[:, e, 1])
+axarr[0].plot(x_obs[:, e, 2])
+axarr[0].set_ylim(-1.1, 1.1)
+axarr[1].plot(X_temp[:, e, 0])
+axarr[1].plot(X_temp[:, e, 1])
+axarr[1].plot(X_temp[:, e, 2])
+axarr[1].set_ylim(-1.1, 1.1)
+plt.savefig(_RESULT_PATH + '%s' % timestamp + '_PositionPlot.png')
+# plt.show()
+
+plt.close()
+f, axarr = plt.subplots(1, 2, figsize=(15, 6))
+axarr[0].plot(np.arctan2(x_obs[:, e, 1], x_obs[:, e, 0]))
+axarr[1].plot(np.arctan2(X_temp[:, e, 1], X_temp[:, e, 0]))
+plt.savefig(_RESULT_PATH + '%s' % timestamp + '_ArcTanPlot.png')
+# plt.show()
